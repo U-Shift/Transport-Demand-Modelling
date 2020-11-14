9387ff5ee54b561a75f5752ae7dddf4e4be061b3
Gabriel Valença <69808511+valenca13@users.noreply.github.com>
2020-11-14 17:24:42 +0000

    script added to folder

---
+++ Code/GLM_R_script.R
@@ -0,0 +1,167 @@
+# INSTITUTO SUPERIOR TECNICO, UNIVERSITY OF LISBON
+# TRANSPORT DEMAND MODELLING COURSE, PROF. FILIPE MOURA
+
+# GENERALISED LINEAR MODELS
+
+# Example: Analyze the number o accidents
+# Accident data from California (1993 to 1998) and Michigan (1993 to 1997) 
+# Variables:
+
+## ACCIDENT: Count on injury accidents over observation period
+## STATE: indicator variable for state: 
+    ## 0: California
+    ## 1: Michigan
+## AADT1: Average annual daily traffic on major road
+## AADT2: Average annual daily traffic on minor road
+## Median: Median width on major road in feet
+## DRIVE: Number of driveways within 250 ft of intersection center
+
+
+# Import Libraries
+
+# Library used for reading excel files
+library(readxl)
+# Library used for summary statistics
+library(skimr)
+# Library used in data science to perform exploratory data analysis
+library(tidyverse) 
+# Library used for goodness of fit parameters
+library(vcd)
+# Library used for goodness of fit
+library(car)
+# Library used for goodness of fit
+library(rcompanion)
+# Library used for the Vuong test
+library(pscl)
+
+# Set working directory
+
+setwd("G:/O meu disco/TDM - Lecture R/Generalised Linear Models")
+
+# Import dataset
+
+dataset <- read_excel("TDM_GZLM_CALMICH_Example.xlsX")
+
+# Let us take a look at the summary and structure of the dataset
+
+summary(dataset)
+
+str(dataset)
+
+# Note: The dataset looks weird. Use the view function to take a better look at the dataset
+
+view(dataset)
+
+# Treat the dataset
+
+# The best way to treat the dataset is to import it again. Use the following steps:
+  ## 1. Go to "Import Dataset" on the "Environment" window at the upper right display;
+  ## 2. Click on "From excel";
+  ## 3. Check if "First row as names" is checked;
+  ## 4. Put the number of rows you want to skip and click on "import"
+  ## 5. This will generate a code, which you can copy and use it the next time you open the file.
+
+# Therefore, here is the following code:
+  TDM_GZLM_CALMICH_Example <- read_excel("TDM_GZLM_CALMICH_Example.xlsx", skip = 5)
+ 
+ ## Transform "TDM_GZLM_CALMICH_Example" into a dataframe to better manage the dataset
+
+ df <- data.frame(TDM_GZLM_CALMICH_Example)
+ view(df)
+ 
+# Now we can analyze the descriptive statistics of the dataframe
+ 
+ str(df)
+ 
+ skim(df)
+
+# Take a look at the histograms of the variables
+
+hist(df$ACCIDENT, xlab = "ACCIDENT", main = "Histogram of ACCIDENT") 
+hist(df$STATE, xlab = "STATE", main = "Histogram of STATE")
+hist(df$AADT1, xlab = "AADT1", main = "Histogram of AADT1")
+hist(df$AADT2, xlab = "AADT2", main = "Histogram of AADT2")
+hist(df$MEDIAN, xlab = "MEDIAN", main = "Histogram of MEDIAN")
+hist(df$DRIVE, xlab = "DRIVE", main = "Histogram of DRIVE")
+
+# Plot the density function of the variable ACCIDENTS
+plot(density(df$ACCIDENT), main="Density estimate of ACCIDENTS")
+
+
+# As the dependent variable is "count data", as has discrete values, then a poisson distribution should be more adequate.
+# Poisson assumption: 
+  # But first, take a look at the mean and the variance of the Dependent variable. 
+  # Check if they are equal to each other. 
+mean(df$ACCIDENT)
+
+#Coefficient of variance:
+
+var(df$ACCIDENT)/mean(df$ACCIDENT)
+
+
+# Try estimating goodness of fit parameter for the PDF of ACCIDENT. Use the Maximum Likelihood method.
+gf<-goodfit(df$ACCIDENT,type= "poisson",method= "ML")
+summary(gf)
+
+
+# Now let us run the many possible models
+ 
+ model1 = glm(ACCIDENT ~ as.factor(STATE) + AADT1 + AADT2 + MEDIAN + DRIVE, family = poisson(link = "log"), data = df, method = "glm.fit")
+ 
+# There are many families and links that can be used, depending on the characteristics of your data.
+ 
+# Family                Link
+# gaussian              identity
+# binomial              logit, probit, cloglog
+# poisson               log, identity, sqrt
+# gamma                 inverse, identity, log
+# inverse.gaussian      1/mu^2
+
+ summary(model1)
+ 
+# Note: If we obtain: 
+ ## residuals > degrees of freedom (overdispersion); 
+ ## residuals < degrees of freedom (underdispersion);
+ ## residuals = degrees of freedom (mean = variance).
+ 
+# In overdispersion, the estimates are reliable but the standard errors tend to be smaller. 
+ 
+with(model1, cbind(res.deviance = deviance, df = df.residual, p= pchisq(deviance, df.residual, lower.tail = FALSE)))
+#botar o lagrange
+# Anova(model1, type = "II", test = "LR") 
+
+nagelkerke(model1)
+
+#Note: Ho: The model fits the data. Therefore, we want to not reject the null hypothesis (pvalue > 0.05).
+
+# Let us correct the standard errors with an overdispersed poisson 
+ 
+ model2 = glm(ACCIDENT ~ STATE + AADT1 + AADT2 + MEDIAN + DRIVE, family = quasipoisson(link = "log"), data = df)
+ 
+ summary(model2)
+ 
+ with(model2, cbind(res.deviance = deviance, df = df.residual, p= pchisq(deviance, df.residual, lower.tail = FALSE)))
+ 
+ Anova(model2, type = "II", test = "LR") 
+ 
+ nagelkerke(model2)
+ 
+ # Note: The estimates are the same, but the standard errors have increased because they are 
+ # adjusted by the scale parameter  
+
+# Let us try the negative binomial distribution
+ 
+ model3 = glm(as.factor(ACCIDENT) ~ STATE + AADT1 + AADT2 + MEDIAN + DRIVE, family = binomial(link = logit), data = df)
+ 
+ #pq tem que colocar como fator?
+ summary(model3)
+  
+ with(model3, cbind(res.deviance = deviance, df = df.residual, p= pchisq(deviance, df.residual, lower.tail = FALSE)))
+
+ Anova(model3, type = "II", test = "LR") 
+ 
+ nagelkerke(model3)
+
+
+ 
+ 

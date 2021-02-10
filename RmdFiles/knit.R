fe0a3a13dcd02622c40555941653532fbe404b54
temospena <temospena@gmail.com>
2021-02-10 13:41:03 +0000

    kint parameters
    and instrunctions
    #15

--- RmdFiles/knit.R
+++ RmdFiles/knit.R
@@ -1,15 +1,6 @@
 # Knit and render Rmd files with correct related paths, for data, for md output, and for figure plots.
 
 
-#Slides
-rmarkdown::render(
-  input = "RmdFiles/Slides.Rmd",
-  knit_root_dir = getwd(),
-  output_dir = getwd(),
-  clean = T
-)
-
-
 ##run each one separately
 
 # name = "1-ExploratoryDataAnalysis"
@@ -20,11 +11,12 @@ name = "3-FactorAnalysis"
 # name = "6-SpatialModels" 
 # name = "7-PanelModels" #não tem figs
 # name = "9-HazardBasedModels"
-# name = "0-InstallR.Rmd" #não tem figs
+# name = "0-InstallR" #não tem figs
 # name = "00-RMarkdownReports" #não tem figs
+# name = "Slides" #não tem figs
 
-file.rename(paste0("RmdFiles/", name, ".md"), paste0(name, ".md"))
-file.rename(paste0("RmdFiles/RmdFiles/", name, "/"), paste0("RmdFiles/", name, "/"))
+file.rename(paste0("RmdFiles/", name, ".md"), paste0(name, ".md")) #mover md
+file.rename(paste0("RmdFiles/RmdFiles/", name, "/"), paste0("RmdFiles/", name, "/")) #mover figuras
 
 
 #manually delete folder "RmdFiles/RmdFiles/" that is empty.

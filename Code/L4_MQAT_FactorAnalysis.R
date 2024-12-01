be9bf40a7da74b47b4b8bf7be5645442e251cd53
Gabriel Valença <69808511+valenca13@users.noreply.github.com>
2024-12-01 22:37:28 +0000

    Update L4_MQAT_FactorAnalysis.R

--- Code/L4_MQAT_FactorAnalysis.R
+++ Code/L4_MQAT_FactorAnalysis.R
@@ -36,7 +36,7 @@
 #' 
 #' #### Adequate Sample Size: 
 #' * At least 10 variables
-#' * n < 50 (Unacceptable); n > 200 (recommended); 1:10 or higher
+#' * n < 50 (Unacceptable); n > 200 (recommended); 10 observations per variable or higher
 #' * It is recommended to use continuous variables. 
 #' * If your data contains categorical variables, you should transform them to dummy variables. 
 #' 
@@ -69,11 +69,16 @@ class(df) #type of data
 str(df)
 
 #' ##### Select a dataset with the first variables explained above
-df = df [,1:24]
+df = df[,1:24]
 #' 
 #' ##### Check summary statistics of variables
 descriptive_stats <- dfSummary(df)
 view(descriptive_stats)
+
+str(df)
+
+# Remove categorical variables or transform them into dummy (factor)
+df = select(df,c(-DWELCLAS, -ISEX, -RAGE10, -HEADH))
 
 #' > **Note:** I used a different library of the MLR chapter for performing the summary statistics. "R" allows you to do the same or similar tasks with different packages. 
 #' 
@@ -92,12 +97,13 @@ df<-data.frame(df, row.names = 1)
 n_obs <- nrow(df)
 n_vars <- ncol(df)
 ratio=n_obs/n_vars
-print(n_obs)
-print(n_vars)
-print(ratio)
+
+n_obs
+n_vars
+ratio
 #' 
 #' * **Normality**
-shapiro.test(df$DWELCLAS)  # Test normality of each variable
+shapiro.test(df$CHILD13)  # Test normality of each variable
 
 # Make normality tests to all variables
 normality_tests <- sapply(df, function(x) {
@@ -124,14 +130,14 @@ normality_pvalues
 
 #If you want to focus on which variables do not follow normality, you can filter the output:
 non_normal_vars <- names(normality_pvalues[normality_pvalues < 0.05])
-print(non_normal_vars)
+non_normal_vars
 
 #If you want to focus on which variables follow normality, you can filter the output:
 normal_vars <- names(normality_pvalues[normality_pvalues >= 0.05])
-print(normal_vars)
+normal_vars
 
 #Example of histogram
-hist(df$H18)
+hist(df$INCOME)
 
 #NOTE: 
 #There are no normally distributed variables in the dataset.
@@ -139,7 +145,9 @@ hist(df$H18)
 #but for Maximum Likelihood (ML) extraction, normality is preferable.
 
 #' * **Linearity Between Variables**
-pairs(df)  # Pairwise scatter plots for all variables
+pairs(df[,1:10], lower.panel = NULL)  # Pairwise scatter plots for all variables
+
+
 plot(df$BEDROOM, df$H18, 
      main = "Scatterplot of Variable1 vs Variable2", 
      xlab = "Variable1", 
@@ -151,6 +159,7 @@ abline(a = 0, b = 1, col = "red", lty = 2)
 #' * **Correlations between variables** 
 #' Correlation matrix
 corr_matrix <- cor(df, method = "pearson")
+
 corrplot(corr_matrix, method = "square")
 
 #' The **Bartlett sphericity test** 
@@ -174,22 +183,21 @@ KMO(corr_matrix)
 #'The KMO test also provides an MSA (Measure of Sampling Adequacy) for each variable, 
 #' indicating how well each variable fits with the others in terms of common variance.
 #'Here are some important interpretations:
-#'  - Good MSA values (≥ 0.70): Variables such as INCOME (0.86), HEMPLOY (0.89), and RAGE10 (0.80) are well-suited for factor analysis and share sufficient common variance with the other variables.
-#'  - Mediocre MSA values (0.50 ≤ MSA < 0.70): Variables like DWELCLAS (0.73), HSIZE (0.59), and PARK (0.59) are marginal for factor analysis.
+#'  - Good MSA values (≥ 0.70): Variables such as INCOME (0.86), HEMPLOY (0.87) are well-suited for factor analysis and share sufficient common variance with the other variables.
+#'  - Mediocre MSA values (0.50 ≤ MSA < 0.70): Variables like HSIZE (0.57), and PARK (0.55) are marginal for factor analysis.
 #'      These variables have some shared variance with the other variables but are not as strong contributors.
-#'  - Low MSA values (< 0.50): Variables like CHILD13 (0.32), AVADUAGE (0.37), IAGE (0.40), and HEADH (0.46) have very low MSA scores. 
+#'  - Low MSA values (< 0.50): Variables like CHILD13 (0.30), and PARKSIZE (0.48) have very low MSA scores. 
 #'      These variables do not share enough common variance with the others and might be poorly suited for factor analysis.
 
 #'Note2: You exclude the low MSA value variables: Code: 
-new_df <- df %>% select(-CHILD13, -AVADUAGE, -IAGE, -HEADH, -BEDROOM, -AREA)
+new_df <- df %>% select(-CHILD13, -PARKSIZE)
 
 corr_matrix <- cor(new_df, method = "pearson")
 KMO(corr_matrix)
 
 #' **Check for multicollinearity**. Multicollinearity happens when variables are very highly correlated 
 #' (e.g., correlations above 0.9), which can distort factor analysis results.
-vif(lm(df$DWELCLAS ~ ., data = new_df))  # Replace Variable1 with a dependent variable
-
+vif(lm(df$INCOME ~ ., data = new_df))  # Replace Variable1 with a dependent variable
 
 
 #'Note: If the VIF of any variable is greater than 10, multicollinearity may be an issue. 
@@ -200,7 +208,7 @@ vif(lm(df$DWELCLAS ~ ., data = new_df))  # Replace Variable1 with a dependent va
 #' 
 #' **1. Parallel Analysis**
 #' 
-num_factors = fa.parallel(new_df, fm = "ml", fa = "PAF")
+num_factors = fa.parallel(new_df, fm = "ml", fa = "fa")
 
 #' 
 #' > **Note:** `fm` = factor math; `ml` = maximum likelihood; `fa` = factor analysis
@@ -236,15 +244,18 @@ plot(df_pca,type="lines", npcs = 17, las = 2)
 #' * **Model 3**: Rotation Oblimin
 
 # No rotation
-df_factor <- factanal(df_new, factors = 4, rotation = "none", scores=c("regression"), fm = "ml")
+df_factor <- factanal(new_df, factors = 4, rotation = "none", scores=c("regression"), fm = "ml")
 # Rotation Varimax
-df_factor_var <- factanal(df_new, factors = 4, rotation = "varimax", scores=c("regression"), fm = "ml")
+df_factor_var <- factanal(new_df, factors = 4, rotation = "varimax", scores=c("regression"), fm = "ml")
 # Rotiation Oblimin
-df_factor_obl <- factanal(df_new, factors = 4, rotation = "oblimin", scores=c("regression"), fm = "ml")
+df_factor_obl <- factanal(new_df, factors = 4, rotation = "oblimin", scores=c("regression"), fm = "ml")
 
 #' 
 #' Let's print out the results of `df_factor_var`, and have a look. 
+
+print(df_factor, digits=2, cutoff=0.3, sort=TRUE)
 print(df_factor_var, digits=2, cutoff=0.3, sort=TRUE) #cutoff of 0.3 due to the sample size is higher than 350 observations.
+print(df_factor_obl, digits=2, cutoff=0.3, sort=TRUE)
 
 #' 
 #' > **Note:** 
@@ -252,12 +263,12 @@ print(df_factor_var, digits=2, cutoff=0.3, sort=TRUE) #cutoff of 0.3 due to the 
 #' Varimax assigns orthogonal rotation, and oblimin assigns oblique rotation.
 #' 
 #' 
-#' Plot factor 1 against factor 2, and compare the results of different rotations
+#' Plot factor 3 against factor 4, and compare the results of different rotations
 #' 
 #' * **No Rotation**
 plot(
-  df_factor$loadings[, 1],
-  df_factor$loadings[, 2],
+  df_factor$loadings[, 3],
+  df_factor$loadings[, 4],
   xlab = "Factor 1",
   ylab = "Factor 2",
   ylim = c(-1, 1),
@@ -265,7 +276,7 @@ plot(
   main = "No rotation"
 )
 abline(h = 0, v = 0)
-load <- df_factor$loadings[, 1:2]
+load <- df_factor$loadings[, 3:4]
 text(
   load,
   names(df),
@@ -276,8 +287,8 @@ text(
 #' 
 #' * **Varimax rotation**
 plot(
-  df_factor_var$loadings[, 1],
-  df_factor_var$loadings[, 2],
+  df_factor_var$loadings[, 3],
+  df_factor_var$loadings[, 4],
   xlab = "Factor 1",
   ylab = "Factor 2",
   ylim = c(-1, 1),
@@ -285,7 +296,7 @@ plot(
   main = "Varimax rotation"
 )
 abline(h = 0, v = 0)
-load <- df_factor_var$loadings[, 1:2]
+load <- df_factor_var$loadings[, 3:4]
 text(
   load,
   labels = names(df),
@@ -297,8 +308,8 @@ text(
 #' 
 #' * **Oblimin Rotation**
 plot(
-  df_factor_obl$loadings[, 1],
-  df_factor_obl$loadings[, 2],
+  df_factor_obl$loadings[, 3],
+  df_factor_obl$loadings[, 4],
   xlab = "Factor 1",
   ylab = "Factor 2",
   ylim = c(-1, 1),
@@ -306,7 +317,7 @@ plot(
   main = "Oblimin rotation"
 )
 abline(h = 0, v = 0)
-load <- df_factor_obl$loadings[, 1:2]
+load <- df_factor_obl$loadings[, 3:4]
 text(
   load,
   labels = names(df),

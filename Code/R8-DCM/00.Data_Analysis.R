f362bef71b0caaee7d154bdfd29e61428d859392
Gabriel Valença <69808511+valenca13@users.noreply.github.com>
2025-01-06 17:42:38 +0000

    Codigo_MNL

--- Code/R8-DCM/00.Data_Analysis.R
+++ Code/R8-DCM/00.Data_Analysis.R
@@ -1,13 +1,6 @@
 # This code has been adapted from the MNL_RP example of the documentation of the "apollo" library, 
 # found at: https://www.apollochoicemodelling.com/examples.html
 
-### Clear memory
-rm(list = ls())
-
-### Load Apollo library
-library(apollo)
-library(skimr)
-
 # We are going to use the example dataset from Apollo. 
 # The dataset is to be used for discrete choice modelling. 
 # Data comes from 500 individuals, each with two revealed preferences (RP) observation, 
@@ -44,6 +37,17 @@ library(skimr)
 # time_car: Travel time (in minutes) of mode car.
 # time_rail: Travel time (in minutes) of mode rail.
 
+### Clear memory
+rm(list = ls()) #optional but recommended 
+
+### Load Apollo library
+library(apollo)
+library(skimr)
+
+# Initialise code
+apollo_initialise()
+
+
 # Loading data from package
 database = apollo_modeChoiceData
 # for data dictionary, use ?apollo_modeChoiceData
@@ -54,33 +58,131 @@ skim(database)
 # Use only RP data
 database = subset(database,database$RP==1)
 
+# Exploratory Data Analysis
 
-# ============================================================================ #
-# EXPLORE DATA
-# ============================================================================ #
 # Mode choice
 counts <- table(database$choice)
 
-
 barplot(counts, 
-        main="Mode choice",
-        xlab="Choice", 
+        main="Revealed preferences of mode choices",
+        xlab="Mode choice",
+        ylab="Nº respondents",
         names.arg = c("car", "bus", "air", "rail"),
-        col = rainbow(4))
+        col = rainbow(4),
+        ylim = c(0,350),
+        las = 1)
 
 
 # Travel times
-hist(database$time_car, xlab = "Minutes", main = "Travel Time - Car")
-hist(database$time_bus, xlab = "Minutes", main = "Travel Time - Bus")
-hist(database$time_air, xlab = "Minutes", main = "Travel Time - Air")
-hist(database$time_rail, xlab = "Minutes", main = "Travel Time - Rail")
+hist(database$time_car, xlab = "Minutes", main = "Travel Time - Car", las = 1, ylim = c(0,350))
+hist(database$time_bus, xlab = "Minutes", main = "Travel Time - Bus", las = 1, ylim = c(0,500), xlim = c(0,500))
+hist(database$time_air, xlab = "Minutes", main = "Travel Time - Air", las = 1, xlim = c(0,100))
+hist(database$time_rail, xlab = "Minutes", main = "Travel Time - Rail", las = 1, ylim = c(0,400), xlim = c(0,200))
 
 # Travel costs
-hist(database$cost_car, xlab = "GBP", main = "Travel Cost - Car")
-hist(database$cost_bus, xlab = "GBP", main = "Travel Cost - Bus")
-hist(database$cost_air, xlab = "GBP", main = "Travel Cost - Air")
-hist(database$cost_rail, xlab = "GBP", main = "Travel Cost - Rail")
+hist(database$cost_car, xlab = "GBP", main = "Travel Cost - Car", las = 1, ylim = c(0,250))
+hist(database$cost_bus, xlab = "GBP", main = "Travel Cost - Bus", las = 1, ylim = c(0,200))
+hist(database$cost_air, xlab = "GBP", main = "Travel Cost - Air", las = 1, xlim = c(0,120))
+hist(database$cost_rail, xlab = "GBP", main = "Travel Cost - Rail", las = 1, ylim = c(0,200), xlim = c(0,80))
 
 # Income
-hist(database$income, xlab = "GBP per annum", main = "Individual Income")
+hist(database$income, xlab = "GBP per annum", main = "Individual Income", las = 1, ylim = c(0,120), xlim = c(0, 80000))
 
+# ============================================================================ #
+
+# MULTINOMIAL LOGIT MODEL
+
+
+# Set core controls
+apollo_control = list(
+  modelName       = "MNL_SimpleExample",
+  modelDescr      = "Multinomial Logit Model",
+  indivID         = "ID", 
+  outputDirectory = "output"
+)
+
+# Define model parameters                               
+# Vector of parameters, including any that are kept fixed in estimation
+apollo_beta=c(asc_car   = 0,
+              asc_bus   = 0,
+              asc_air   = 0,
+              asc_rail  = 0,
+              b_tt_car  = 0,
+              b_tt_bus  = 0,
+              b_tt_air  = 0,
+              b_tt_rail = 0)
+
+# Vector with names (in quotes) of parameters to be kept fixed at their starting 
+# value in apollo_beta, use apollo_beta_fixed = c() if none
+
+apollo_fixed = c("asc_car")
+
+# Group and validate inputs
+
+apollo_inputs = apollo_validateInputs() #The function runs a number of checks and produces a consolidated list of model inputs
+
+# "apollo_inputs" checks if all inputs required for the model are defined, it will fail otherwise.  
+# (apollo_control, apollo_beta, apollo_fixed, and the database)
+
+
+# Define model and likelihood function
+
+apollo_probabilities = function(apollo_beta, apollo_inputs, functionality="estimate"){
+
+# obs:  While the other functions we have encountered are part of the package, 
+# "apollo_probabilities" needs to be defined by the user as
+# it is specific to the model to be estimated. 
+    
+  # Attach inputs and detach after function exit
+  apollo_attach(apollo_beta, apollo_inputs)
+  on.exit(apollo_detach(apollo_beta, apollo_inputs))
+  
+  # Create list of probabilities P
+  P = list()
+  
+  # List of utilities: these must use the same names as in mnl_settings, order is irrelevant
+  V = list()
+  V[["car"]]  = asc_car  + b_tt_car  * time_car 
+  V[["bus"]]  = asc_bus  + b_tt_bus  * time_bus 
+  V[["air"]]  = asc_air  + b_tt_air  * time_air  
+  V[["rail"]] = asc_rail + b_tt_rail * time_rail 
+  
+  # Define settings for MNL model component
+  mnl_settings = list(
+    alternatives  = c(car=1, bus=2, air=3, rail=4), 
+    avail         = list(car=av_car, bus=av_bus, air=av_air, rail=av_rail), 
+    choiceVar     = choice,
+    utilities     = V
+  )
+  
+  # Compute probabilities using MNL model
+  P[["model"]] = apollo_mnl(mnl_settings, functionality)
+  
+  # Take product across observation for same individual
+  P = apollo_panelProd(P, apollo_inputs, functionality)
+  
+  # Prepare and return outputs of function
+  P = apollo_prepareProb(P, apollo_inputs, functionality)
+  return(P)
+}
+
+# Estimate the model
+
+model = apollo_estimate(apollo_beta,
+                        apollo_fixed, 
+                        apollo_probabilities, 
+                        apollo_inputs)
+
+
+# Print Outputs
+# To the screen
+apollo_modelOutput(model, modelOutput_settings = list(printDataReport =T,
+                                                      printFixed =T,
+                                                      printPVal =1) )
+
+# To a file
+apollo_saveOutput(model)
+
+# ============================================================================ #
+
+# NESTED LOGIT MODEL
